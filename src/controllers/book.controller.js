5ae165a5c4d8cf0d91289197194834b6f96969ba
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-04-19 19:29:10 +0700

    Implementing add new book

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -1,9 +1,27 @@
-export const getAllBooks = async function () {};
+import { Book } from "../models/book.model.js";
 
-export const addNewBook = async function () {};
+export const getAllBooks = async function (req, res) {};
 
-export const getBook = async function () {};
+export const addNewBook = async function (req, res) {
+  try {
+    const newBook = await Book.create(req.body);
+    res.status(201).json({
+      status: "success",
+      createdAt: new Date().toISOString(),
+      data: {
+        book: newBook,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
 
-export const updateBook = async function () {};
+export const getBook = async function (req, res) {};
 
-export const deleteBook = async function () {};
+export const updateBook = async function (req, res) {};
+
+export const deleteBook = async function (req, res) {};
