b744381d6a04fa0535f875c43afef90dda632f47
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-04-19 16:04:48 +0700

    Creating book model schema

---
+++ src/models/book.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    unique: true,
+    required: [true, "The book title can not be empty"],
+  },
+  author: {
+    type: String,
+    required: [true, "The book author can not be empty"],
+  },
+  pages: {
+    type: Number,
+    required: [true, "The book pages can not be empty"],
+  },
+  description: {
+    type: String,
+    required: [true, "The book description can not be empty"],
+  },
+  publishDate: {
+    type: Date,
+    required: [true, "The book publish date can not be empty"],
+  },
+});
+
+export const Book = mongoose.model("Book", bookSchema);
