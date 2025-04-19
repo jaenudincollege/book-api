ea3a74924218fab9e529515fa31f2337eca724f4
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-04-19 15:52:14 +0700

    Setup the server

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
