e9e600ba03f1f0758fac8afbb535c0c395071171
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-04-28 12:31:43 +0700

    Implement error handling in production and development mode

--- src/controllers/error.controller.js
+++ src/controllers/error.controller.js
@@ -1,10 +1,38 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const sendErrorDevelopment = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProduction = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: err,
+      message: "Something went wrong!",
+    });
+  }
+};
+
 export const globalError = (err, req, res, next) => {
   err.status = err.status || "error";
   err.statusCode = err.statusCode || 500;
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDevelopment(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProduction(err, res);
+  }
+
   next();
 };
