8bd72d93d5bd2a07a14fdae48eb23fd8c3c84faa
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-04-28 21:24:30 +0700

    Implementing login for user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,15 @@
 import { User } from "../models/user.model.js";
 import { catchAsync } from "../utils/catchAsync.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { AppError } from "../utils/AppError.js";
+dotenv.config();
+
+const signinToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 export const signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -9,12 +19,32 @@ export const signup = catchAsync(async (req, res, next) => {
     confirmPassword: req.body.confirmPassword,
   });
 
+  const token = signinToken(newUser._id);
+
   res.status(201).json({
     status: "success",
+    token,
     data: {
       newUser,
     },
   });
 });
 
-export const login = catchAsync();
+export const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password!", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Incorrect email or password!", 401));
+  }
+
+  const token = signinToken(user._id);
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+});
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -49,4 +49,11 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
 export const User = mongoose.model("User", userSchema);
