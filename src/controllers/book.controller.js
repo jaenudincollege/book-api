a059652df14c004fe2a6bd450983002d2e30f64b
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-04-28 11:30:24 +0700

    Add catchAsync utility  for error handing

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -1,87 +1,57 @@
 import { Book } from "../models/book.model.js";
+import { catchAsync } from "../utils/catchAsync.js";
 
-export const getAllBooks = async function (req, res) {
-  try {
-    const books = await Book.find();
-    res.status(200).json({
-      status: "success",
-      results: books.length,
-      data: {
-        books,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
-};
+export const getAllBooks = catchAsync(async function (req, res) {
+  const books = await Book.find();
 
-export const addNewBook = async function (req, res) {
-  try {
-    const newBook = await Book.create(req.body);
-    res.status(201).json({
-      status: "success",
-      createdAt: new Date().toISOString(),
-      data: {
-        book: newBook,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    results: books.length,
+    data: {
+      books,
+    },
+  });
+});
 
-export const getBook = async function (req, res) {
-  try {
-    const book = await Book.findById(req.params.id);
-    res.status(201).json({
-      status: "success",
-      data: {
-        book,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      message: error.message,
-    });
-  }
-};
+export const addNewBook = catchAsync(async function (req, res) {
+  const newBook = await Book.create(req.body);
+  res.status(201).json({
+    status: "success",
+    createdAt: new Date().toISOString(),
+    data: {
+      book: newBook,
+    },
+  });
+});
 
-export const updateBook = async function (req, res) {
-  try {
-    const book = await Book.findByIdAndUpdate(req.params.id, req.body);
-    res.status(201).json({
-      status: "success",
-      message: "Book updated successfully",
-      data: {
-        book,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      message: error.message,
-    });
-  }
-};
+export const getBook = catchAsync(async function (req, res) {
+  const book = await Book.findById(req.params.id);
+  res.status(201).json({
+    status: "success",
+    data: {
+      book,
+    },
+  });
+});
 
-export const deleteBook = async function (req, res) {
-  try {
-    const book = await Book.findByIdAndDelete(req.params.id);
-    res.status(201).json({
-      status: "success",
-      message: "Book deleted successfully",
-      data: {
-        book,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      message: error.message,
-    });
-  }
-};
+export const updateBook = catchAsync(async function (req, res) {
+  const book = await Book.findByIdAndUpdate(req.params.id, req.body);
+  res.status(201).json({
+    status: "success",
+    message: "Book updated successfully",
+    data: {
+      book,
+    },
+  });
+});
+
+export const deleteBook = catchAsync(async function (req, res) {
+  const book = await Book.findByIdAndDelete(req.params.id);
+  res.status(201).json({
+    status: "success",
+    message: "Book deleted successfully",
+    data: {
+      book,
+    },
+  });
+});
